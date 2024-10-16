d6b837204e7ed40a150dd2d6b0ea341b28547597
Bo Thompson <bo_l_thompson@yahoo.com>
2024-10-16 12:23:55 -0700

    imagine counting in binary manually, couldn't be me

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -14,17 +14,6 @@ class ExperimentalCube {
       out vec4 vColor;
 
       void main() {
-        const vec3 vertices[] = vec3[8](
-          vec3(0, 0, 0),  //0
-          vec3(0, 0, 1),  //1
-          vec3(0, 1, 0),  //2
-          vec3(0, 1, 1),  //3
-          vec3(1, 0, 0),  //4
-          vec3(1, 0, 1),  //5
-          vec3(1, 1, 0),  //6
-          vec3(1, 1, 1)   //7
-        );
-
         const ivec3 indices[] = ivec3[12](
           ivec3(0, 2, 6),
           ivec3(0, 6, 4),
@@ -40,7 +29,8 @@ class ExperimentalCube {
           ivec3(2, 7, 6)
         );
         
-        vec4 v = vec4(vertices[indices[gl_InstanceID][gl_VertexID]], 1.0);
+        int i = indices[gl_InstanceID][gl_VertexID];
+        vec4 v = vec4(i >> 2 & 1, i >> 1 & 1, i & 1, 1.0);
         vColor = v;
         v.xyz -= 0.5;
         gl_Position = P * MV * v;
